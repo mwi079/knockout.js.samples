75c4c8b2f3b203b91fb881dfebcff2264b53c69b
malcolm.wilkie <malcolm.wilkie@and.digital>
2022-11-15 11:40:49 +0000

    upgraded jquery

--- 02-templates-and-lists/model.js
+++ 02-templates-and-lists/model.js
@@ -1,37 +1,36 @@
 $(document).ready(function() {
   // Raw catalog data - would come from the server
-  var availableMeals = [
+  const availableMeals = [
       { mealName: "Standard (sandwich)", price: 0 },
       { mealName: "Premium (lobster)", price: 34.95 },
       { mealName: "Ultimate (whole zebra)", price: 290 }
   ];
   
   // Class to represent a row in the reservations grid
-  var seatReservation = function(name) {
+  const seatReservation = function(name) {
       this.name = name;
       this.availableMeals = availableMeals;
       this.meal = ko.observable(availableMeals[0]);
       this.remove = function() { viewModel.seats.remove(this) }
-      this.formattedPrice = ko.dependentObservable(function() {
-          var price = this.meal().price;
+      this.formattedPrice = ko.computed(function() {
+          let price = this.meal().price;
           return price ? "$" + price.toFixed(2) : "None";        
       }, this);
   }
   
   // Overall viewmodel for this screen, along with initial state
-  var viewModel = {
+  const viewModel = {
       seats: ko.observableArray([
-          new seatReservation("Steve"),
-          new seatReservation("Bert")
+          new seatReservation("Malcolm"),
+          new seatReservation("Layla")
       ])
       ,addSeat: function() {
           this.seats.push(new seatReservation());   
       }
   };
-  
-  viewModel.totalSurcharge = ko.dependentObservable(function() {
-     var total = 0;
-     for (var i = 0; i < this.seats().length; i++)
+  viewModel.totalSurcharge = ko.computed(function() {
+     let total = 0;
+     for (let i = 0; i < this.seats().length; i++)
          total += this.seats()[i].meal().price;
      return total;
   }, viewModel);
